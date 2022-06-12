67d775919e6fcbc7031b2386be3b8cb3aedeed68
«Mansur» <sozd.space@gmail.com>
2022-06-12 20:09:18 +0300

    начал карзину

---
+++ controllers/basket.controller.js
@@ -0,0 +1,41 @@
+const Basket = require("../models/Basket.model");
+
+module.exports.basketContrl = {
+  addBasket: async (req, res) => {
+    try {
+      await Basket.create({
+        userId: user._id,
+        totalPrice: 0,
+        amout: 0,
+      });
+    } catch (error) {
+      res.json({ error: "Ошибка при добавлении корзины" });
+    }
+  },
+
+  addBookToBasket: async (req, res) => {
+    const BookPrice = await Drug.findById(req.params.bookId);
+    try {
+      await Basket.findByIdAndUpdate(req.params.BasketId, {
+        $push: {
+          bookId: req.body.bookId,
+          totalPrice: (totalPrice = totalPrice + BookPrice),
+        },
+      });
+    } catch (error) {
+      res.json({ error: "Ошибка при добавлении книги" });
+    }
+  },
+
+  removeBookForBasket: async (req, res) => {
+    try {
+      await Basket.findByIdAndUpdate(req.params.BasketId, {
+        $pull: {
+          bookId: req.body.bookId,
+        },
+      });
+    } catch (error) {
+      res.json({ error: "Ошибка при удалении книги" });
+    }
+  },
+};
---
+++ models/Basket.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const basketSchema = mongoose.basketSchema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  bookId: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Book",
+    },
+  ],
+  totalPrice: Number,
+  amout: { type: Number, default: 0 },
+});
+
+const Basket = mongoose.model("Basket", basketSchema);
+module.exports = Basket;

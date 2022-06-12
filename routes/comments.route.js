77017592822ab4c48c019196c19370e1359155cf
Muhammad <tasuhanow@mail.ru>
2022-06-12 17:47:18 +0300

    добавил роуты, которые Сулиман удалил

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -1,6 +1,10 @@
 const {Router} = require("express");
-const { discussionController } = require("../controllers/Discussion.controller");
+const { commentsController } = require("../controllers/comments.controllers");
 
 const router = Router()
+
+router.post("/", commentsController.postComments)
+router.get("/", commentsController.getAllComments)
+router.get("/:commentId", commentsController.getCommentsById)
 
 module.exports = router
--- routes/discussions.route.js
+++ routes/discussions.route.js
@@ -1,6 +1,17 @@
-const {Router} = require("express");
-const { discussionController } = require("../controllers/Discussion.controller");
+const { Router } = require("express");
+const {
+  discussionController,
+} = require("../controllers/Discussion.controller");
 
-const router = Router()
+const router = Router();
 
-module.exports = router
+router.post("/", discussionController.postDiscussion);
+router.get("/", discussionController.getDiscussion);
+router.get(
+  "/:discussionId",
+  discussionController.getDiscussionById
+);
+router.patch("/add/answers/:discussionId", discussionController.addAnswer);
+router.patch("/add/watched/:discussionId", discussionController.addWatched)
+
+module.exports = router;

3f4c00bb8c6a102f26ce7a8f455141718f0850e2
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-06-13 11:07:48 +0300

    добавил контроллер для закладок

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { userController } = require("../controllers/users.controllers");
+const authmiddleware = require("../middlewares/auth.middleware");
 
 const router = Router();
 
@@ -7,5 +8,6 @@ router.post("/", userController.registerUser);
 router.post("/login", userController.login);
 router.get("/", userController.getAlUsers);
 router.get("/:id", userController.getUserById);
+router.patch("/:id/bookmarks", authmiddleware, userController.addBookmarks);
 
 module.exports = router;

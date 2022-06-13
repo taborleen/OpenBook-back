6665ab600d847a7f72c4a1022b09f8c453fa19b0
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-06-13 11:32:44 +0300

    добавил в индекс роуты корзины

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,13 @@ const genresRoutes = require("./genres.routes");
 const authorsRoutes = require("./authors.routes");
 const reviewsRoutes = require("./reviews.routes");
 const usersRoutes = require("./users.routes");
-
+const basketRoutes = require("./basket.route");
 
 router.use("/books", bookRoutes);
 router.use("/genres", genresRoutes);
 router.use("/authors", authorsRoutes);
 router.use("/reviews", reviewsRoutes);
 router.use("/users", usersRoutes);
+router.use("/basket", basketRoutes);
 
 module.exports = router;

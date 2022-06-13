b606fa9b4ac830ddd269f5cc6ab2bd774c7b5c3d
Suleiman <80757260+Sa1tama54@users.noreply.github.com>
2022-06-13 11:28:16 +0300

    внес правки

---
+++ controllers/book.controller.js
@@ -0,0 +1,74 @@
+const Book = require("../models/Book.modele");
+
+module.exports.bookController = {
+  createBook: async (req, res) => {
+    const {
+      name,
+      genres,
+      author,
+      description,
+      price,
+      publicationYear,
+      amountPages,
+      size,
+      coverType,
+      left,
+      discount,
+      rating,
+      image,
+    } = req.body;
+
+    try {
+      const book = await Book.create({
+        name,
+        genres,
+        author,
+        description,
+        price,
+        publicationYear,
+        amountPages,
+        size,
+        coverType,
+        left,
+        discount,
+        rating,
+        image,
+      });
+      return res.json(book);
+    } catch (e) {
+      return res.json("ошибка");
+    }
+  },
+
+  getAllBook: async (req, res) => {
+    try {
+      const book = await Book.find({}).populate(
+        "genres author publishingHouse"
+      );
+      res.json(book);
+    } catch (error) {
+      res.json({ error: "Ошибка при вызове" });
+    }
+  },
+
+  getOneBook: async (req, res) => {
+    try {
+      const book = await Book.findById(req.params.id).populate(
+        "genres author publishingHouse"
+      );
+      res.json(book);
+    } catch (error) {
+      res.json({ error: "Ошибка при вызове" });
+    }
+  },
+  getBooksOnGenre: async (req, res) => {
+    try {
+      const book = await Book.find({
+        genres: req.params.id,
+      }).populate("genres author publishingHouse");
+      res.json(book);
+    } catch (error) {
+      res.json({ error: "ошибка при просмотре книг определенного жанра" });
+    }
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ const { Router } = require("express");
 
 const router = Router();
 
-// const bookRoutes = require("./book.routes");
+const bookRoutes = require("./book.routes");
 const genresRoutes = require("./genres.routes");
 const authorsRoutes = require("./authors.routes");
 const reviewsRoutes = require("./reviews.routes");
 const usersRoutes = require("./users.routes");
 
 
-// router.use("/books", bookRoutes);
+router.use("/books", bookRoutes);
 router.use("/genres", genresRoutes);
 router.use("/authors", authorsRoutes);
 router.use("/reviews", reviewsRoutes);
